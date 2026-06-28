1aabca1d4db4c89c207ecb084f8f8303518803f6
Gaurav Dhall <gauravdhall35@gmail.com>
2026-06-28 21:37:01 +0530

    Time: 1 ms (77.87%), Space: 42.1 MB (88.42%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,33 +1,34 @@
 class Solution {
-    public boolean findHappy(int n,Map <Integer,Integer> hash){
-
-        if(hash.containsKey(n)){
-            return false;
-        }
-
-        int original=n;
-
-        
+    public int nextNum(int n){
         int sum=0;
         while(n!=0){
             int ld=n%10;
-            System.out.println(ld);
+           
             sum+=ld*ld;
-            System.out.println(sum);
+
             n/=10;
 
-            
         }
-        System.out.println(sum);
-        if(sum==1){
+       
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+
+        do{
+            slow=nextNum(slow);
+            
+            fast=nextNum(nextNum(fast));
+           
+
+            
+        }while(slow!=fast);
+
+        if(slow==1){
             return true;
         }
-        hash.put(original,1);
-       return findHappy(sum,hash);
-    } 
-    public boolean isHappy(int n) {
-        Map <Integer,Integer> hash= new HashMap<>();
-        Boolean isHappyNo=findHappy(n,hash);
-        return isHappyNo;
+
+        return false;
     }
 }
