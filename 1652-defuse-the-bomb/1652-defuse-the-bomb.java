64f1b97c0e84ff4c4f787a4027d211e849a8b38a
Gaurav Dhall <gauravdhall35@gmail.com>
2026-06-29 21:46:11 +0530

    Time: 1 ms (63.22%), Space: 43.6 MB (91.92%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,50 +1,51 @@
 class Solution {
-    public int findSum(int [] arr,int k,int i,int n){
-        
-        
-        int sum=0;
-        if(k>0){
-            for(int j=1;j<=k;j++){
-           sum+= arr[i+j];
-        }
-        }
-        else if(k<0){
-            for(int j=n+i-1;j>=n+i+k;j--){
-                sum+=arr[j];
-            }
-            
-        }
-        else{
-            sum=0;
-        }
-        
-        return sum;
-    }
     public int[] decrypt(int[] code, int k) {
-        int n=code.length;
+        int n= code.length;
 
         int [] arr= new int [2*n];
-
         for(int i=0;i<arr.length;i++){
             arr[i]=code[i%n];
         }
 
+
         if(k>0){
-            for(int i=0;i<n;i++){
-            code[i]=findSum(arr,k,i,n);
-        }
+            int l=0;
+            int r=l+k;
+
+            while(r<arr.length){
+                if(l==n){
+                    break;
+                }
+                int sum=0;
+                for(int i=l+1;i<=r;i++){
+                    sum+=arr[i];
+                }
+                code[l]=sum;
+                l++;
+                r++;
+            }
         }
         else if(k<0){
-            for(int i=n-1;i>=0;i--){
-                code[i]=findSum(arr,k,i,n);
+            int r=2*n-1;
+            int l=r+k;
+            while(l>=0){
+                if(r==n-1){
+                    break;
+                }
+                int sum=0;
+                for(int i=l;i<r;i++){
+                    sum+=arr[i];
+                }
+                code[r-n]=sum;
+                r--;
+                l--;
             }
         }
         else{
             for(int i=0;i<n;i++){
-                code[i]=findSum(arr,k,i,n);
+                code[i]=0;
             }
         }
-        
 
         return code;
     }
