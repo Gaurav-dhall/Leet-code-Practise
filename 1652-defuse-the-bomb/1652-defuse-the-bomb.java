9cc66ed22587df5bbf60e9547f7137e5be4f99cf
Gaurav Dhall <gauravdhall35@gmail.com>
2026-06-29 22:08:40 +0530

    Time: 0 ms (100%), Space: 44.1 MB (13.46%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,50 +1,72 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n= code.length;
 
-        int [] arr= new int [2*n];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=code[i%n];
+        int n = code.length;
+
+        int[] arr = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i++) {
+            arr[i] = code[i % n];
         }
 
+        if (k > 0) {
 
-        if(k>0){
-            int l=0;
-            int r=l+k;
+            int l = 1;
+            int r = k;
 
-            while(r<arr.length){
-                if(l==n){
-                    break;
-                }
-                int sum=0;
-                for(int i=l+1;i<=r;i++){
-                    sum+=arr[i];
-                }
-                code[l]=sum;
+            int sum = 0;
+
+            // Initial window sum
+            for (int i = l; i <= r; i++) {
+                sum += arr[i];
+            }
+
+            for (int i = 0; i < n; i++) {
+
+                code[i] = sum;
+
+                sum -= arr[l];
                 l++;
+
                 r++;
+                sum += arr[r];
             }
+
         }
-        else if(k<0){
-            int r=2*n-1;
-            int l=r+k;
-            while(l>=0){
-                if(r==n-1){
-                    break;
-                }
-                int sum=0;
-                for(int i=l;i<r;i++){
-                    sum+=arr[i];
-                }
-                code[r-n]=sum;
-                r--;
-                l--;
+
+        else if (k < 0) {
+
+            k = -k;
+
+            int l = n - k;
+            int r = n - 1;
+
+            int sum = 0;
+
+            // Initial window sum
+            for (int i = l; i <= r; i++) {
+                sum += arr[i];
             }
+
+            for (int i = 0; i < n; i++) {
+
+                code[i] = sum;
+
+                sum -= arr[l];
+                l++;
+
+                r++;
+                sum += arr[r];
+            }
+
         }
-        else{
-            for(int i=0;i<n;i++){
-                code[i]=0;
+
+        else {
+
+            for (int i = 0; i < n; i++) {
+                code[i] = 0;
             }
+
         }
 
         return code;
