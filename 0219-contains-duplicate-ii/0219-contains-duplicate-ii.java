aba11296b17015c56fd0b095d0ad5c4c918e9213
Gaurav Dhall <gauravdhall35@gmail.com>
2026-07-01 19:01:56 +0530

    Time: 24 ms (51.47%), Space: 75.8 MB (90.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,22 +3,22 @@ class Solution {
         int i=0;
         int j=0;
         int n=nums.length;
-        Map<Integer,Integer> hash= new HashMap<>();
+        Set<Integer> hash= new HashSet<>();
 
         while(j<n){
 
-            if(hash.containsKey(nums[j])){
+            if(hash.contains(nums[j])){
                 return true;
             }
             if(j>=i+k){
-                hash.put(nums[j],j);
+                hash.add(nums[j]);
                 hash.remove(nums[i]);
                 i++;
                 
                 j++;
             }
             else{
-            hash.put(nums[j],j);
+            hash.add(nums[j]);
             j++;
             }
         }
