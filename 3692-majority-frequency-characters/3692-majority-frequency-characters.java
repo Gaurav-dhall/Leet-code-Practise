fddebbca937a88b077c00f9e072f1f69e7c9de16
Gaurav Dhall <gauravdhall35@gmail.com>
2026-07-02 18:38:34 +0530

    Time: 1 ms (100%), Space: 44.5 MB (77.82%) - LeetHub

--- 3692-majority-frequency-characters/3692-majority-frequency-characters.java
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.java
@@ -1,29 +1,33 @@
 class Solution {
     public String majorityFrequencyGroup(String s) {
         int [] hash = new int[26];
-        int [] size= new int[200];
+        int [] size= new int[101];
         StringBuilder ans = new StringBuilder();
 
         for(char ch:s.toCharArray()){
             hash[ch-'a']++;
         }
-
+            int maxDist=Integer.MIN_VALUE;
+        int ind=0;
         for(int i=0;i<26;i++){
             if(hash[i]!=0){
                  size[hash[i]]++;
+                 if(size[hash[i]]>maxDist){
+                    maxDist=Integer.max(maxDist,size[hash[i]]);
+            ind=hash[i];
+                 }
+                 else if(size[hash[i]]==maxDist){
+                    if(hash[i]>ind){
+                        ind=hash[i];
+                    }
+                 }
             }
            
         }
 
-        int maxDist=Integer.MIN_VALUE;
-        int ind=0;
+    
 
-        for(int i=0;i<200;i++){
-            if(size[i]>=maxDist){
-                maxDist=Integer.max(maxDist,size[i]);
-            ind=i;
-            }
-        }
+        
 
         for(int i=0;i<26;i++){
             if(hash[i]==ind){
