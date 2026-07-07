2dcf3e5aed66b113bf8aaf3d495a8aa715090f29
Gaurav Dhall <gauravdhall35@gmail.com>
2026-07-07 14:11:06 +0530

    Time: 6 ms (97.93%), Space: 48.2 MB (98.79%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,16 +1,35 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int i=nums.length-2;
+        int min = Integer.MAX_VALUE;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            min=Math.min(min,nums[i]);
+        }
+
+        int [] hash= new int[20003];
+
+        for(int i=0;i<nums.length;i++){
+            hash[nums[i]-min]++;
+        }
+
+        int i=0;
+        for(int j=0;j<hash.length;j++){
+           
+                while(hash[j]>0){
+                    nums[i]=j+min;
+                    i++;
+                    hash[j]--;
+                }
+            
+        }
+        int k=nums.length-2;
         int sum=0;
-
-
-        while(i>=0){
-            sum+=nums[i];
-            i=i-2;
-
+        while(k>=0){
+            sum+=nums[k];
+            k=k-2;
         }
 
         return sum;
+
     }
 }
