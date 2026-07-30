304d9027881d38b6bbea27555369121b1f390128
Gaurav Dhall <gauravdhall35@gmail.com>
2026-07-30 20:58:46 +0530

    Time: 1 ms (29.67%), Space: 43.3 MB (28.23%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,39 +1,33 @@
 class Solution {
     public int minimumPushes(String word) {
-        int ones=1;
-        int twos=1;
-        int threes=1;
-        int fours=1;
+        int unique=0;
         int[] hash = new int[26];
         int minKey=0;
 
 
         for(int i=0;i<word.length();i++){
-            if(hash[word.charAt(i)-'a']!=0){
-                    minKey+=hash[word.charAt(i)-'a'];
-                    continue;
+           
+                   
+            if(hash[word.charAt(i)-'a']==0){
+                if(unique<8){
+                    hash[word.charAt(i)-'a']=1;
                 }
-            if(ones<=8&&hash[word.charAt(i)-'a']==0){
+                else if(unique<16){
+                    hash[word.charAt(i)-'a']=2;
+                }
+                else if(unique<24){
+                    hash[word.charAt(i)-'a']=3;
+                }
+                else{
+                    hash[word.charAt(i)-'a']=4;
+                }
+
+                unique++;
+            }
+
+             minKey+=hash[word.charAt(i)-'a'];
+             
                 
-                hash[word.charAt(i)-'a']=1;
-                minKey+=hash[word.charAt(i)-'a'];
-                ones++;
-            }
-            if(twos<=8&&hash[word.charAt(i)-'a']==0){
-                hash[word.charAt(i)-'a']=2;
-                minKey+=hash[word.charAt(i)-'a'];
-                twos++;
-            }
-            if(threes<=8&&hash[word.charAt(i)-'a']==0){
-                hash[word.charAt(i)-'a']=3;
-                minKey+=hash[word.charAt(i)-'a'];
-                threes++;
-            }
-            if(fours<=8&&hash[word.charAt(i)-'a']==0){
-                hash[word.charAt(i)-'a']=4;
-                minKey+=hash[word.charAt(i)-'a'];
-                fours++;
-            }
             
 
         }
