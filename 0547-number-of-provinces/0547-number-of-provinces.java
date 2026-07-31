8d881a054908ce1d0bdf66a923eae78c0269d88d
Gaurav Dhall <gauravdhall35@gmail.com>
2026-07-31 14:46:54 +0530

    Time: 1 ms (86.66%), Space: 47.1 MB (88.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,26 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int[] vis = new int[n];
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(0);
-        vis[0] = 1;
-        int province = 0;
-
-        while (!q.isEmpty()) {
-            while (!q.isEmpty()) {
-                int curr = q.poll();
-
-                for (int j = 0; j < isConnected[curr].length; j++) {
-                    if (isConnected[curr][j] == 1 && vis[j] == 0) {
-                        q.offer(j);
-                        vis[j] = 1;
-                    }
-                }
-            }
-            province++;
-            for (int i = 0; i < n; i++) {
-                if (vis[i] == 0) {
-                    q.offer(i);
-                    break;
-                }
+    int province=0;
+     
+    public void dfs(int [][] isConnected,int [] vis,int curr){
+        for(int i=0;i<isConnected[curr].length;i++){
+            if(vis[i]==0&&isConnected[curr][i]==1){
+                vis[i]=1;
+                dfs(isConnected,vis,i);
             }
         }
-
+       
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+       int[] vis= new int[n];
+        for(int i=0;i<vis.length;i++){
+            if(vis[i]==0){
+                vis[i]=1;
+                dfs(isConnected,vis,i);
+                province++;
+            }
+        }
         return province;
+       
     }
 }
