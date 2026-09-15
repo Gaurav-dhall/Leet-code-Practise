7f8772f0af902f2aa7544ac0b7ca6838da38dd52
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-15 21:04:24 +0530

    Time: 14 ms (25.62%), Space: 46.5 MB (44.19%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,59 +1,83 @@
-class El{
-    int time;
-    int row;
-    int col;
+import java.util.*;
 
-    El(int _t,int _r,int _c){
-        this.time=_t;
-        this.row=_r;
-        this.col=_c;
+class Disjoint {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    Disjoint(int n) {
+        for (int i = 0; i < n; i++) {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUpar(int u) {
+        if (parent.get(u) == u) {
+            return u;
+        }
+
+        parent.set(u, findUpar(parent.get(u)));
+        return parent.get(u);
+    }
+
+    public void union(int u, int v) {
+        int pu = findUpar(u);
+        int pv = findUpar(v);
+
+        if (pu == pv) {
+            return;
+        }
+
+        if (size.get(pu) < size.get(pv)) {
+            parent.set(pu, pv);
+            size.set(pv, size.get(pu) + size.get(pv));
+        } else {
+            parent.set(pv, pu);
+            size.set(pu, size.get(pu) + size.get(pv));
+        }
     }
 }
 
-
 class Solution {
     public int swimInWater(int[][] grid) {
-        int n=grid.length;
+        int n = grid.length;
+        Disjoint dj = new Disjoint(n * n);
 
-        PriorityQueue<El> pq= new PriorityQueue<>((a,b)->a.time-b.time);
-        pq.offer(new El(grid[0][0],0,0));
+        // pos[value] = {row, col}
+        int[][] pos = new int[n * n][2];
 
-        int [] vis= new int[n*n];
-        vis[0]=1;
-        int ans=0;
-
-        while(!pq.isEmpty()){
-            El curr= pq.peek();
-            pq.poll();
-            int currTime=curr.time;
-            int r=curr.row;
-            int c=curr.col;
-
-            if(r==n-1&&c==n-1){
-                ans=currTime;
-                break;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pos[grid[i][j]][0] = i;
+                pos[grid[i][j]][1] = j;
             }
-
-            vis[n*r+c]=1;
-
-            if(r<n-1&&vis[n*(r+1)+c]==0){
-                pq.offer(new El(Math.max(currTime,grid[r+1][c]),r+1,c));
-            }
-
-             if(r>0&&vis[n*(r-1)+c]==0){
-                pq.offer(new El(Math.max(currTime,grid[r-1][c]),r-1,c));
-            }
-
-            if(c>0&&vis[n*r+c-1]==0){
-                pq.offer(new El(Math.max(currTime,grid[r][c-1]),r,c-1));
-            }
-
-            if(c<n-1&&vis[n*r+c+1]==0){
-                pq.offer(new El(Math.max(currTime,grid[r][c+1]),r,c+1));
-            }
-
         }
 
-        return ans;
+        boolean[][] active = new boolean[n][n];
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int t = 0; t < n * n; t++) {
+            int r = pos[t][0];
+            int c = pos[t][1];
+            active[r][c] = true;
+
+            int node = r * n + c;
+
+            for (int[] dir : dirs) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && active[nr][nc]) {
+                    int neighbourNode = nr * n + nc;
+                    dj.union(node, neighbourNode);
+                }
+            }
+
+            if (dj.findUpar(0) == dj.findUpar(n * n - 1)) {
+                return t;
+            }
+        }
+
+        return -1;
     }
 }
