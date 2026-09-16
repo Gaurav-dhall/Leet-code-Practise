8631aba8cf588145708b207a847b2322a6a23800
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-16 13:42:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,6 @@
 class Solution {
 
-    public int helper(int n,int [] dp){
-        if(n<=1){
-            return n;
-        }
-
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        dp[n]=helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
-    }
+    
    
     public int fib(int n) {
      int [] dp = new int[n+1];
@@ -19,8 +8,18 @@ class Solution {
      for(int i=0;i<n+1;i++){
         dp[i]=-1;
      }
+     dp[0]=0;
+     if(n>0) dp[1]=1;
 
-    return helper(n,dp);
+     if(n<=1){
+        return dp[n];
+     }
+
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+
+    return dp[n];
      
     }
 }
