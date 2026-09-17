b4061c3105a0e60357da081c0419843d526a17fd
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-17 18:24:43 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.25%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,5 @@
 class Solution {
-    public int helper(int [] nums,int i,int first,int[] dp){
-        if(i==first){
-            return nums[i];
-        }
-        if(i<first){
-            return 0;
-        }
-
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-
-        int pick=nums[i]+helper(nums,i-2,first,dp);
-        int notPick=0+helper(nums,i-1,first,dp);
-
-        return dp[i]= Math.max(pick,notPick);
-
-    }
+ 
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1){
