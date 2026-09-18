3279eb4c666f0fac0b5d737bc855919d781ef2af
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-18 21:00:59 +0530

    Time: 0 ms (100%), Space: 43.7 MB (11.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,51 +1,57 @@
 class Solution {
-    public int helper(int r, int c,int[][] obstacleGrid,int [][]dp){
-        if(r==0&&c==0){
-            return 1;
-        }
+   
 
-        int left=0;
-        int top=0;
-
-        if(r>0&&obstacleGrid[r-1][c]!=1){
-            if(dp[r-1][c]!=-1){
-                top=dp[r-1][c];
-            }
-            else{
-
-            top=helper(r-1,c,obstacleGrid,dp);
-            }
-        }
-
-        if(c>0&&obstacleGrid[r][c-1]!=1){
-            if(dp[r][c-1]!=-1){
-                left=dp[r][c-1];
-            }
-            else{
-
-            left=helper(r,c-1,obstacleGrid,dp);
-            }
-        }
-
-        return dp[r][c]= top+left;
-        
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        int [][] dp = new int[m][n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
-        }
-
-        if(obstacleGrid[m-1][n-1]==1){
+        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1){
             return 0;
         }
 
-        return helper(m-1,n-1,obstacleGrid,dp);
+        int [] prev=new int[n];
+        int start = 1;
+
+   
+
+        for (int i = 0; i < m; i++) {
+            int [] temp= new int[n];
+            temp[0]=(prev[0]==0&&i>0)?0:1;
+            start=temp[0];
+            for (int j = 0; j < n; j++) {
+               
+
+                
+
+                if(obstacleGrid[i][j]==1){
+                    temp[j]=0;
+                    start=0;
+                    continue;
+                }
+
+                if(j==0){
+                   
+                    temp[0]=(prev[0]==0&&i>0)?0:1;
+                    start=temp[0];
+                    
+                   
+                   continue;
+                }
+
+               
+
+                temp[j]=prev[j]+start;
+                start=temp[j];
+
+
+                
+
+                
+
+            }
+            prev=temp;
+        }
+
+        return prev[n-1];
     }
 }
