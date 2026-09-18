f73399d47e012104d6108f65f6286f0371fea1a7
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-18 12:58:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,42 +1,35 @@
 class Solution {
 
-    public int helper(int r, int c,int [][] dp) {
-        if (r == 0 && c == 0) {
-            return 1;
-        }
-
-        int left = 0;
-        int top = 0;
-
-        if (c > 0) {
-            if(dp[r][c-1]!=-1){
-                left=dp[r][c-1];
-            }
-            else{
-            left = helper(r, c - 1,dp);
-            }
-        }
-
-        if (r > 0) {
-            if(dp[r-1][c]!=-1){
-                top=dp[r-1][c];
-            }
-            else{
-            top = helper(r - 1, c,dp);
-            }
-        }
-
-        return dp[r][c]=left+top;
-
-    }
 
     public int uniquePaths(int m, int n) {
-        int [][] dp= new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int left = 0;
+                int top = 0;
+
+                if (j > 0) {
+                  
+                        left = dp[i][j-1];
+                    
+                }
+
+                if (i > 0) {
+                   
+                        top = dp[i - 1][j];
+                
+                }
+
+                 dp[i][j] = left + top;
+
             }
         }
-      return  helper(m - 1, n - 1,dp);
+        return dp[m-1][n-1];
     }
 }
