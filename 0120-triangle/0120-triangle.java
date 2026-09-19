814a7a923246f46273fc399685da48dce3bda61f
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-19 16:51:56 +0530

    Time: 13 ms (7.25%), Space: 46.4 MB (23.21%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,48 +1,43 @@
 class Solution {
-    public int helper(List<List<Integer>> triangle,int r,int c,List<List<Integer>> dp){
+    public int helper(List<List<Integer>> triangle, int r, int c, List<List<Integer>> dp) {
 
-        if(r==0&&c==0){
-            return triangle.get(0).get(0);
-        }
-        if(dp.get(r).get(c)!=-1){
-            return dp.get(r).get(c);
-        }
-        int left=Integer.MAX_VALUE;
-        int right=Integer.MAX_VALUE;
-
-
-        if(c>0){
-            left=helper(triangle,r-1,c-1,dp);
-        }
-
-        if(c<triangle.get(r).size()-1){
-            right= helper(triangle,r-1,c,dp);
-        }
-
-        dp.get(r).add(c,triangle.get(r).get(c)+Math.min(left,right));
         return dp.get(r).get(c);
-        
-       
+
     }
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size();
-        int minAns=Integer.MAX_VALUE;
-        List<List<Integer>> dp= new ArrayList<>();
-        for(int i=0;i<n;i++){
+        int n = triangle.size();
+
+        List<List<Integer>> dp = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             dp.add(new ArrayList<>());
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<triangle.get(i).size();j++){
-                dp.get(i).add(-1);
+        dp.get(0).add(triangle.get(0).get(0));
+        for (int i = 1; i < triangle.size(); i++) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+
+                if (j > 0) {
+                    left = dp.get(i-1).get(j - 1);
+                }
+
+                if (j < triangle.get(i).size() - 1) {
+                    right = dp.get(i-1).get(j);
+                }
+
+                 dp.get(i).add(j,triangle.get(i).get(j)+Math.min(left,right));
             }
         }
-        for(int i=0;i<triangle.get(n-1).size();i++){
-           minAns=Math.min(minAns,helper(triangle,n-1,i,dp)) ;
+
+        int minAns=Integer.MAX_VALUE;
+
+        for(int i=0;i<dp.get(n-1).size();i++){
+            minAns=Math.min(minAns,dp.get(n-1).get(i));
         }
 
         return minAns;
-
 
     }
 }
