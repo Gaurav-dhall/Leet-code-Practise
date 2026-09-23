fea4c04b1066acb408eba14d2b016d6c86cbd6c0
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-23 18:44:58 +0530

    Time: 109 ms (37.05%), Space: 46 MB (34.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int f(int [] nums,int target,int i,int sum){
+    public int f(int [] nums,int target,int i,int sum,int [][] dp){
         if(i==0){
          
              if(sum+nums[0]==target&&sum-nums[0]==target){
@@ -10,14 +10,34 @@ class Solution {
             }
             return 0;
         }
-        int pos=f(nums,target,i-1,sum+nums[i]);
-        int neg=f(nums,target,i-1,sum-nums[i]);
+
+        if(sum>=0&&dp[i][sum]!=-1){
+            return dp[i][sum];
+        }
+        int pos=f(nums,target,i-1,sum+nums[i],dp);
+        int neg=f(nums,target,i-1,sum-nums[i],dp);
+
+        if(sum>=0){
+            dp[i][sum]= pos+neg;
+        }
 
         return pos+neg;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        int [][] dp= new int[n][sum+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<sum+1;j++){
+                dp[i][j]=-1;
+            }
+        }
 
-       return f(nums,target,n-1,0);
+
+
+       return f(nums,target,n-1,0,dp);
     }
 }
