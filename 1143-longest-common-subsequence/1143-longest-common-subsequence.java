13d47b58835161fbeedf7b6e720e7816c26c0644
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-25 13:00:18 +0530

    Time: 18 ms (93.65%), Space: 42.5 MB (99.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,40 +1,33 @@
 class Solution {
-    public int f(int i1, int i2, String text1, String text2, int[][] dp) {
-        if (i1 < 0 || i2 < 0) {
-            return 0;
-        }
-
-        if (dp[i1][i2] != -1) {
-            return dp[i1][i2];
-        }
-        if (text1.charAt(i1) == text2.charAt(i2)) {
-            return dp[i1][i2] = 1 + f(i1 - 1, i2 - 1, text1, text2, dp);
-        }
-        return dp[i1][i2] = 0 + Math.max(f(i1 - 1, i2, text1, text2, dp), f(i1, i2 - 1, text1, text2, dp));
-    }
 
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[] prev = new int[m + 1];
-        
 
-        
+        int[] prev = new int[m + 1];
 
         for (int i1 = 1; i1 < n + 1; i1++) {
-            int [] temp =new int[m+1];
+
+            int diagonal = 0;   // dp[i-1][0]
+
             for (int i2 = 1; i2 < m + 1; i2++) {
-                int i=i1-1;
-                int j=i2-1;
+
+                int i = i1 - 1;
+                int j = i2 - 1;
+
+                int temp = prev[i2]; // save dp[i-1][i2]
+
                 if (text1.charAt(i) == text2.charAt(j)) {
-                     temp[i2] = 1 + prev[ i2 - 1];
+                    prev[i2] = 1 + diagonal;
                 }
-                else temp[i2] = 0 + Math.max(prev[i2], temp[ i2- 1]);
+                else {
+                    prev[i2] = Math.max(prev[i2], prev[i2 - 1]);
+                }
+
+                diagonal = temp; // old prev[i2] becomes diagonal for next j
             }
-            prev=temp;
         }
 
-        return prev[ m ] ;
-
+        return prev[m];
     }
 }
