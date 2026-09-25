40cf40d5ccf8a87d974a3261d3818d89e0c92aa3
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-25 11:48:45 +0530

    Time: 23 ms (40.52%), Space: 53.7 MB (85.95%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,42 @@
 class Solution {
-    public int f(int i1,int i2,String text1,String text2,int [][] dp){
-        if(i1<0||i2<0){
+    public int f(int i1, int i2, String text1, String text2, int[][] dp) {
+        if (i1 < 0 || i2 < 0) {
             return 0;
         }
 
-        if(dp[i1][i2]!=-1){
+        if (dp[i1][i2] != -1) {
             return dp[i1][i2];
         }
-        if(text1.charAt(i1)==text2.charAt(i2)){
-            return dp[i1][i2]= 1+f(i1-1,i2-1,text1,text2,dp);
+        if (text1.charAt(i1) == text2.charAt(i2)) {
+            return dp[i1][i2] = 1 + f(i1 - 1, i2 - 1, text1, text2, dp);
         }
-        return dp[i1][i2]= 0+Math.max(f(i1-1,i2,text1,text2,dp),f(i1,i2-1,text1,text2,dp));
+        return dp[i1][i2] = 0 + Math.max(f(i1 - 1, i2, text1, text2, dp), f(i1, i2 - 1, text1, text2, dp));
     }
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-        int [][] dp= new int[n][m];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i1 = 1; i1 < n + 1; i1++) {
+            for (int i2 = 1; i2 < m + 1; i2++) {
+                int i=i1-1;
+                int j=i2-1;
+                if (text1.charAt(i) == text2.charAt(j)) {
+                     dp[i1][i2] = 1 + dp[i1 -1 ][ i2 - 1];
+                }
+                else dp[i1][i2] = 0 + Math.max(dp[i1 - 1][i2], dp[i1][ i2- 1]);
             }
         }
-        return f(n-1,m-1,text1,text2,dp);
+
+        return dp[n ][ m ] ;
 
     }
 }
