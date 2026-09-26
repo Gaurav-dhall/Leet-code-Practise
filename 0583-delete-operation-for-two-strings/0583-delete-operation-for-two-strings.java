b225a1f91a1332824ca94ceee3f640dd4faa58ed
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-26 12:40:48 +0530

    Time: 6 ms (94.29%), Space: 46.3 MB (82.93%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,34 +1,23 @@
 class Solution {
-    public int f(String word1,String word2,int i1,int i2,int [][] dp){
-
-        if(i1<0||i2<0){
-            return 0;
-        }
-        if(dp[i1][i2]!=-1){
-            return dp[i1][i2];
-        }
-        if(word1.charAt(i1)==word2.charAt(i2)){
-            return dp[i1][i2]=1+f(word1,word2,i1-1,i2-1,dp);
-        }
-
-        return dp[i1][i2]=Math.max(f(word1,word2,i1-1,i2,dp),f(word1,word2,i1,i2-1,dp));
-    }
+    
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int [][] dp= new int [n+1][m+1];
+        int [] prev= new int [m+1];
         
         for(int i1=1;i1<n+1;i1++){
+            int [] temp = new int[m+1];
             for(int i2=1;i2<m+1;i2++){
                 if(word1.charAt(i1-1)==word2.charAt(i2-1)){
-            dp[i1][i2]=1+dp[i1-1][i2-1];
+            temp[i2]=1+prev[i2-1];
         }
 
-       else dp[i1][i2]=Math.max(dp[i1-1][i2],dp[i1][i2-1]);
+       else temp[i2]=Math.max(prev[i2],temp[i2-1]);
             }
+            prev=temp;
         }
 
-        int sub=dp[n][m];
+        int sub=prev[m];
         return (m-sub)+(n-sub);
     }
 }
