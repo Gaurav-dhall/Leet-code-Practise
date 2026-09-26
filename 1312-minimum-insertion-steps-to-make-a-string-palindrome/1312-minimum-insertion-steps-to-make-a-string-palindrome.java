805a6393d323caf9bcc02855a819ed3662d39018
Gaurav Dhall <gauravdhall35@gmail.com>
2026-09-26 17:43:49 +0530

    Time: 17 ms (94.15%), Space: 47.1 MB (67.55%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,33 @@
 class Solution {
-    public int f(int l,int r,String s,int [][] dp){
-        if(l>r){
+    public int f(int l, int r, String s, int[][] dp) {
+        if (l > r) {
             return 0;
         }
-        if(l==r){
+        if (l == r) {
             return 0;
         }
 
-        if(dp[l][r]!=-1){
+        if (dp[l][r] != -1) {
             return dp[l][r];
         }
-        if(s.charAt(l)==s.charAt(r)){
-            return dp[l][r]= 0+f(l+1,r-1,s,dp);
+        if (s.charAt(l) == s.charAt(r)) {
+            return dp[l][r] = 0 + f(l + 1, r - 1, s, dp);
         }
 
-        return dp[l][r]= 1+Math.min(f(l+1,r,s,dp),f(l,r-1,s,dp));
+        return dp[l][r] = 1 + Math.min(f(l + 1, r, s, dp), f(l, r - 1, s, dp));
     }
+
     public int minInsertions(String s) {
-        int n=s.length();
-        int [][] dp= new int [n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int l = n - 1; l >= 0; l--) {
+            for (int r = l + 1; r < n; r++) {
+                if (s.charAt(l) == s.charAt(r)) {
+                   dp[l][r] = 0 + dp[l + 1][ r - 1];
+                }
+                else dp[l][r] = 1 + Math.min(dp[l + 1][ r], dp[l][ r - 1]);
             }
         }
-        return f(0,n-1,s,dp);
+        return dp[0][ n - 1];
     }
 }
